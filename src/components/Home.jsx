d96e9db27384f2f70bae88f6e4b44753acc5445a
Hitesh Arora <hitesh0627.be21@chitkara.edu.in>
2024-07-20 02:17:13 +0530

    home text updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,46 +1,54 @@
-import React from 'react';
-import { useState, useEffect, useRef } from 'react';
-import { name } from '../constants';
-import Footer from './Footer';
+import React from "react";
+import { useState, useEffect, useRef } from "react";
+import { name } from "../constants";
+import Footer from "./Footer";
 
 const Home = () => {
+  const ref = useRef(0);
+  const [text, setText] = useState("");
 
-	const ref = useRef(0);
-	const [text, setText] = useState('');
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (ref.current < name.length) {
+        ref.current++;
+        setText(() => text + name[ref.current - 1]);
+      }
+    }, 500);
+    return () => clearInterval(interval);
+  }, [text]);
 
-	useEffect(() => {
-		const interval = setInterval(() => {
-			if (ref.current < name.length) {
-				ref.current++;
-				setText(() => text + name[ref.current - 1]);
-			}
-		}, 500);
-		return () => clearInterval(interval);
-	}, [text]);
-
-	return (
-		<div className='area relative z-0 bg-black w-screen h-screen'>
-			<ul class="circles">
-				<li></li>
-				<li></li>
-				<li></li>
-				<li></li>
-				<li></li>
-				<li></li>
-				<li></li>
-				<li></li>
-				<li></li>
-				<li></li>
-			</ul>
-			<div className='hero relative h-[calc(100vh)] flex justify-center items-center text-white' id='hero'>
-				<div className='pt-4 h-36 backdrop-blur-sm rounded-3xl'>
-					<h1 className='text-6xl sm:text-7xl font-extrabold mt-2'>Hi, I'm&nbsp;<span className='text-yellow-200 font-extrabold'>{text}</span></h1>
-					<p className='mt-3 text-xl'>I am passionate about learning Data Structures and Algorithms, adept at building scalable iOS applications, and possess strong communication skills and leadership qualities."</p>
-				</div>      
-			</div>
-			<Footer/>
-		</div>	
-	);
-}
+  return (
+    <div className="area relative z-0 bg-black w-screen h-screen">
+      <ul class="circles">
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+      <div
+        className="hero relative h-[calc(100vh)] flex justify-center items-center text-white"
+        id="hero"
+      >
+        <div className="pt-4 h-36 backdrop-blur-sm rounded-3xl">
+          <h1 className="text-6xl sm:text-7xl font-extrabold mt-2">
+            Hi, I'm&nbsp;
+            <span className="text-yellow-200 font-extrabold">{text}</span>
+          </h1>
+          <p className="mt-3 text-xl">
+            Passionate about DSA, building scalable iOS apps, and skilled in
+            communication and leadership.
+          </p>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
 
 export default Home;
